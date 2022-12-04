eeeabd9cac87728fe1162d7c973eae5151d414e0
Dudewithabrick <118382234+Dudewithabrick@users.noreply.github.com>
2022-12-05 00:03:12 +0100

    Update Random_Name_Picker.py

--- Random_Name_Picker.py
+++ Random_Name_Picker.py
@@ -6,7 +6,7 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-random_num = random.randint(0, len(names))
+random_num = random.randint(0, len(names)-1) # Have to reduce the number by 1 in order to get from length of the list to index of the list. 
 random_name = names[random_num]
 print(f"{random_name} is going to buy the meal today!")
 
